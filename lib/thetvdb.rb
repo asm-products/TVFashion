f3bbc390320cdb26da8a602cd571034b5412842e
Gareth McDonald <mcdonagg@gmail.com>
2014-12-19 09:05:48 -0700

    Setup images to use cloudinary for upload

--- lib/thetvdb.rb
+++ lib/thetvdb.rb
@@ -34,7 +34,12 @@ class Thetvdb
   end
 
   def lookup_actors(show_id)
-    self.class.get("/#{@api_key}/series/#{show_id}/actors.xml")["Actors"]["Actor"]
+    results = self.class.get("/#{@api_key}/series/#{show_id}/actors.xml")
+      if results.blank? or results['Actors'].blank?
+        return []
+      else
+        results["Actors"]['Actor']
+      end
   end
 
   def lookup_banners(show_id)
@@ -50,7 +55,6 @@ class Thetvdb
   end
 
   def add_show(show, api, id)
-    puts show.id
     show_data = api.lookup_show(id)
     show.name = show_data['SeriesName']
     show.overview = show_data['Overview']
@@ -65,33 +69,51 @@ class Thetvdb
     show.rating_count = show_data['RatingCount'].to_i
     show.runtime = show_data['Runtime'].to_i
     show.status = show_data['Status']
-    show.banner = Cloudinary::Uploader.upload('http://thetvdb.com/banners/'+show_data['banner'], :public_id => 'Shows/'+show.id.to_s+'/Banner')['public_id'] unless show_data['banner'].blank?
-    show.fanart = 'http://thetvdb.com/banners/'+show_data['fanart'] unless show_data['fanart'].blank?
-    show.poster = 'http://thetvdb.com/banners/'+show_data['poster'] unless show_data['poster'].blank?
+    #I may refactor this if statment to set show.banner first then check if image is uploaded and upload if false
+    if Cloudinary::Utils.cloudinary_url('Shows/'+show.tvdb_id+'/Banner').blank?
+      show.banner = Cloudinary::Uploader.upload('http://thetvdb.com/banners/'+show_data['banner'], :public_id => 'Shows/'+show.tvdb_id+'/Banner')['public_id'] unless show_data['banner'].blank?
+    else
+      show.banner = 'Shows/'+show.tvdb_id+'/Banner'
+    end
+    if Cloudinary::Utils.cloudinary_url('Shows/'+show.tvdb_id+'/Fanart').blank?
+      show.fanart = Cloudinary::Uploader.upload('http://thetvdb.com/banners/'+show_data['fanart'], :public_id => 'Shows/'+show.tvdb_id+'/Fanart')['public_id'] unless show_data['fanart'].blank?
+    else
+      show.fanart = 'Shows/'+show.tvdb_id+'/Fanart'
+    end
+    if Cloudinary::Utils.cloudinary_url('Shows/'+show.tvdb_id+'/Poster').blank?
+      show.poster = Cloudinary::Uploader.upload('http://thetvdb.com/banners/'+show_data['poster'], :public_id => 'Shows/'+show.tvdb_id+'/Poster')['public_id'] unless show_data['poster'].blank?
+    else
+      show.poster = 'Shows/'+show.tvdb_id+'/Poster'
+    end
     show.network = show_data['Network']
     show.genre = show_data['Genre'].split('|').reject(&:empty?) unless show_data['Genre'].blank?
     show.last_updated = DateTime.strptime(api.get_datetime(),'%s')
     show.save
-    puts show.id
     return show
   end
 
   def add_actors(show, api, id)
     act_data = api.lookup_actors(id)
+    date_now = DateTime.strptime(api.get_datetime(),'%s')
     act_data.each do |a|
       act = show.actors.new
       act.name = a['Name']
       act.tvdb_id = a['id']
-      act.image = 'http://thetvdb.com/banners/'+a['Image'] unless a['Image'].blank?
+      if Cloudinary::Utils.cloudinary_url('Actors/'+show.tvdb_id+'/'+act.tvdb_id).blank?
+        act.image = Cloudinary::Uploader.upload('http://thetvdb.com/banners/'+a['Image'], :public_id => 'Actors/'+show.tvdb_id+'/'+act.tvdb_id)['public_id'] unless a['Image'].blank?
+      else
+        act.image = 'Actors/'+show.tvdb_id+'/'+act.tvdb_id
+      end
       act.role = a['Role']
       act.sort_order = a['SortOrder'].to_i
-      act.last_updated = show.last_updated
+      act.last_updated = date_now
       act.save
     end
   end
 
   def add_episodes(show, api, id)
     ep_data = api.lookup_all_episodes(id)
+    date_now = DateTime.strptime(api.get_datetime(),'%s')
     ep_data.each do |e|
       unless e['FirstAired'].nil?
         if Date.parse(e['FirstAired']) < Date.today
@@ -106,11 +128,15 @@ class Thetvdb
           ep.overview = e['Overview']
           ep.rating = e['Rating'].to_f
           ep.rating_count = e['RatingCount'].to_i
-          ep.image = 'http://thetvdb.com/banners/'+e['filename'] unless e['filename'].blank?
+          if Cloudinary::Utils.cloudinary_url('Episodes/'+show.tvdb_id+'/'+ep.tvdb_id).blank?
+            ep.image = Cloudinary::Uploader.upload('http://thetvdb.com/banners/'+e['filename'], :public_id => 'Episodes/'+show.tvdb_id+'/'+ep.tvdb_id)['public_id'] unless e['filename'].blank?
+          else
+            ep.image = 'Episodes/'+show.tvdb_id+'/'+ep.tvdb_id
+          end
           ep.image_height = e['thumb_height']
           ep.image_width = e['thumb_width']
           ep.season_id = e['seasonid']
-          ep.last_updated = show.last_updated
+          ep.last_updated = date_now
           ep.directors = e['Director'].split('|').reject(&:empty?) unless e['Director'].blank?
           ep.guest_stars = e['GuestStars'].split('|').reject(&:empty?) unless e['GuestStars'].blank?
           ep.writers = e['Writer'].split('|').reject(&:empty?) unless e['Writer'].blank?
@@ -126,7 +152,13 @@ class Thetvdb
     season_banners = banners.select {|b| b['BannerType2'] == 'season'}
     posters=[]
     for i in 0..season_count
-      posters << 'http://thetvdb.com/banners/'+season_banners.select {|b| b['Season'] == i.to_s}.first['BannerPath']
+      unless season_banners.select {|b| b['Season'] == i.to_s}.blank?
+        poster = 'http://thetvdb.com/banners/'+season_banners.select {|b| b['Season'] == i.to_s}.first['BannerPath']
+      else
+        #put defualt here
+        poster = 'http://thetvdb.com/banners/posters/108611-11.jpg'
+      end
+      posters << Cloudinary::Uploader.upload(poster, :public_id => 'Shows/'+show.tvdb_id+'/Season-'+i.to_s)['public_id']
     end
     show.update_attribute(:season_posters, posters)
   end
